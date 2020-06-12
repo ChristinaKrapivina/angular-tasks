df434b9466961dbf18e503da85150e8842cfaedb
Callista <kalista5015@gmail.com>
2020-06-12 15:37:47 +0300

    Adds all components to parent component

--- src/app/lecture3/lecture3.component.ts
+++ src/app/lecture3/lecture3.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-lecture3',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lecture3.component.scss']
 })
 export class Lecture3Component implements OnInit {
+  evenNumbers:number[] = [];
+  oddNumbers: number[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  numberAdded(number: number) {
+    if(number % 2 === 0) {
+      this.evenNumbers.push(number);
+    } else {
+      this.oddNumbers.push(number);
+    }
   }
 
 }
