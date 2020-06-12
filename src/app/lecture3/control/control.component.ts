d6119565106f01e8de982d419256057209904a62
Callista <kalista5015@gmail.com>
2020-06-12 15:35:35 +0300

    Adds buttons to control component

--- src/app/lecture3/control/control.component.ts
+++ src/app/lecture3/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./control.component.scss']
 })
 export class ControlComponent implements OnInit {
-
+  @Output() onStart = new EventEmitter<number>();
+  number: number = 0;
+  intervalID: number;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  startCount() {
+    this.intervalID = window.setInterval(() => { 
+      this.onStart.emit(
+        this.number += 1
+      )
+    }, 2000);
+  }
+
+  stopCount() {
+    clearInterval(this.intervalID);
+  }
+  
 }
