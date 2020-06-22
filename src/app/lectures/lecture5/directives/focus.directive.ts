cc7fcaf1a4ae44a296c6cf6d4113f0f4d8ecefcb
Callista <kalista5015@gmail.com>
2020-06-22 14:16:47 +0300

    Creates focus directive

--- src/app/lectures/lecture5/directives/focus.directive.ts
+++ src/app/lectures/lecture5/directives/focus.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  // ** 1st Option **
+  //Renderer2,
+  // ** 2nd Option **
+  ElementRef,
+} from '@angular/core';
 
 @Directive({
   selector: '[appFocus]'
 })
-export class FocusDirective {
+export class FocusDirective implements OnInit{
 
-  constructor() { }
+  constructor(
+    // ** 1st Option **
+    //private renderer: Renderer2,
+    // ** 2nd Option **
+    private el: ElementRef
+  ) { }
 
+  ngOnInit() {
+    // ** 1st Option **
+    //this.renderer.selectRootElement('#focusInput').focus();
+    // ** 2nd Option **
+    this.el.nativeElement.focus();
+  }
 }
