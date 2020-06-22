7bb126754d744ec251bb0fb1a4bcd951b80c52a5
Callista <kalista5015@gmail.com>
2020-06-22 14:19:42 +0300

    Adds hover-change directive

--- src/app/lectures/lecture5/directives/hover-change.directive.ts
+++ src/app/lectures/lecture5/directives/hover-change.directive.ts
@@ -1,10 +1,51 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  HostListener,
+  // ** 1st Option **
+  // Renderer2,
+  // ElementRef,
+  // ** 2nd Option **
+  HostBinding,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHoverChange]'
 })
-export class HoverChangeDirective {
+export class HoverChangeDirective implements OnInit{
+  // ** 2nd Option **
+  @HostBinding('style.fontSize') fontSize: string;
+  @HostBinding('class') borderClass: string;
 
-  constructor() { }
+  constructor(
+    // ** 1st Option **
+    // private renderer: Renderer2,
+    // private el: ElementRef
+  ) { }
 
+  ngOnInit() {
+    // ** 1st Option **
+    //this.renderer.addClass(this.el.nativeElement, 'border-info');
+    // ** 2nd Option **
+    this.fontSize = '1rem';
+    this.borderClass = 'border-info';
+  }
+
+  @HostListener('mouseenter') onHover(): void {
+    // ** 1st Option **
+    // this.renderer.addClass(this.el.nativeElement, 'border-danger');
+    // this.renderer.setStyle(this.el.nativeElement, 'font-size', '2rem');
+    // ** 2nd Option **
+    this.fontSize = '2rem';
+    this.borderClass = 'border-danger';
+  }
+
+  @HostListener('mouseleave') onMouseLeave(): void {
+    // ** 1st Option **
+    // this.renderer.removeClass(this.el.nativeElement, 'border-danger');
+    // this.renderer.removeStyle(this.el.nativeElement, 'font-size');
+    // ** 2nd Option **
+    this.fontSize = '1rem';
+    this.borderClass = 'border-info';
+  }
 }
