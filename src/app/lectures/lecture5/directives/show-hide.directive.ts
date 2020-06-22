ca04e76e546a0beb27f8a2fcce8db847062ed93d
Callista <kalista5015@gmail.com>
2020-06-22 14:20:03 +0300

    Adds show-hide directive

--- src/app/lectures/lecture5/directives/show-hide.directive.ts
+++ src/app/lectures/lecture5/directives/show-hide.directive.ts
@@ -1,10 +1,57 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  Input,
+  ViewContainerRef,
+  TemplateRef,
+  EmbeddedViewRef
+} from '@angular/core';
 
 @Directive({
   selector: '[appShowHide]'
 })
 export class ShowHideDirective {
+  private context: boolean;
+  private ifTemplateRef: TemplateRef<any>|null = null;
+  private elseTemplateRef: TemplateRef<any>|null = null;
 
-  constructor() { }
+  private ifViewRef: EmbeddedViewRef<any>|null = null;
+  private elseViewRef: EmbeddedViewRef<any>|null = null;
+  
+  constructor(
+    private viewContainer: ViewContainerRef, 
+    templateRef: TemplateRef<any>
+  ) {
+    this.ifTemplateRef = templateRef;
+  }
 
+  @Input()
+  set appShowButton(condition: boolean) {
+    this.context = condition;
+    this.updateView();
+  }
+  @Input()
+  set appShowButtonElse(templateRef:TemplateRef<any>|null) {
+    this.elseTemplateRef = templateRef;
+    this.updateView();
+  }
+
+  updateView() {
+    if (this.context) {
+      if (!this.ifViewRef) {
+        this.viewContainer.clear();
+        this.elseViewRef = null;
+        if (this.ifTemplateRef) {
+          this.ifViewRef = this.viewContainer.createEmbeddedView(this.ifTemplateRef);
+        }
+      }
+    } else {
+      if (!this.elseViewRef) {
+        this.viewContainer.clear();
+        this.ifViewRef = null;
+        if (this.elseTemplateRef) {
+          this.elseViewRef = this.viewContainer.createEmbeddedView(this.elseTemplateRef);
+        }
+      }
+    }
+  }
 }
