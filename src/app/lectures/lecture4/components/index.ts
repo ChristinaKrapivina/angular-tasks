db24e189e40711258cc6b374386d7c4126967659
Callista <kalista5015@gmail.com>
2020-06-28 09:47:04 +0300

    Restructures the component

---
+++ src/app/lectures/lecture4/components/index.ts
@@ -0,0 +1,4 @@
+export * from './purchase-creator/purchase-creator.component';
+export * from './purchase-item/purchase-item.component';
+export * from './purchase-list/purchase-list.component';
+export * from './purchase-list/purchase/purchase.component';
