d12d1cadd982aebc0a180eca9fe1b3722d7ed1ab
Callista <kalista5015@gmail.com>
2020-07-02 12:23:44 +0300

    Adds purchase-dashboard component

---
+++ src/app/lectures/lecture4/components/purchase-dashboard/purchase-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-dashboard',
+  templateUrl: './purchase-dashboard.component.html',
+  styleUrls: ['./purchase-dashboard.component.scss']
+})
+export class PurchaseDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
