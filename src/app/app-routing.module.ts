9aa41703210891ad4d7e6444e627b6a2d787a38a
Callista <kalista5015@gmail.com>
2020-07-06 16:09:13 +0300

    Updates routing with page-not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { IntroComponent } from './intro/intro.component';
 import { Lecture2Component } from './lectures/lecture2/lecture2.component';
 import { Lecture3Component } from './lectures/lecture3/lecture3.component';
 import { Lecture5Component } from './lectures/lecture5/lecture5.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'lecture2', component: Lecture2Component },
   { path: 'lecture3', component: Lecture3Component },
   { path: 'lecture5', component: Lecture5Component },
-  { path: '**', redirectTo: '/', pathMatch: 'full' },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: 'page-not-found', pathMatch: 'full' },
 ];
 
 @NgModule({
