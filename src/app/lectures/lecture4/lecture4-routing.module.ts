f44a1f717d680a312e3d8ee4ed978f0155ecf141
Callista <kalista5015@gmail.com>
2020-07-06 16:11:44 +0300

    Updates lecture4 routing module

--- src/app/lectures/lecture4/lecture4-routing.module.ts
+++ src/app/lectures/lecture4/lecture4-routing.module.ts
@@ -2,35 +2,37 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
 
 import { Lecture4Component } from './lecture4.component';
-import { PurchaseCreatorComponent, PurchaseListComponent, PurchaseDetailsComponent, PurchaseDashboardComponent } from './components';
+import { PurchaseCreatorComponent, PurchaseListComponent, PurchaseDetailsComponent, PurchaseDashboardComponent, PurchaseEditorComponent } from './components';
 
 const routes: Routes = [
-     {
-          path: 'lecture4',
-          component: Lecture4Component,
-          children: [
-               {
-                    path: '',
-                    children: [
-                         { path: 'add', component: PurchaseCreatorComponent },
-                         { path: 'purchase', component: PurchaseListComponent },
-                         { path: 'purchase/:purchaseID', component: PurchaseDetailsComponent },
-                         { path: '', component: PurchaseDashboardComponent },
-                    ]
-               }
-          ]
-     }
+  {
+    path: 'lecture4',
+    component: Lecture4Component,
+    children: [
+      { path: 'add', component: PurchaseCreatorComponent },
+      {
+        path: 'purchase',
+        component: PurchaseListComponent,
+        children:[
+          { path: ':purchaseID', component: PurchaseDetailsComponent },
+          { path: ':purchaseID/edit', component: PurchaseEditorComponent },
+        ]
+      },
+      { path: '', component: PurchaseDashboardComponent },
+    ]
+  }
 ];
 
 @NgModule({
-     imports: [RouterModule.forChild(routes)],
-     exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class Lecture4RoutingModule {
-     static components = [
-          Lecture4Component,
-          PurchaseCreatorComponent,
-          PurchaseListComponent,
-          PurchaseDetailsComponent
-     ]
+  static components = [
+    Lecture4Component,
+    PurchaseCreatorComponent,
+    PurchaseListComponent,
+    PurchaseDetailsComponent,
+    PurchaseEditorComponent
+  ]
 }
