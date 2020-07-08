d0e6ad4cefb2a64359caf7196f9471ff5200a330
Callista <kalista5015@gmail.com>
2020-07-08 16:14:08 +0300

    Adds login, logout buttons and functionality

--- src/app/lectures/lecture4/lecture4.component.ts
+++ src/app/lectures/lecture4/lecture4.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services';
 
 @Component({
   selector: 'app-lecture4',
   templateUrl: './lecture4.component.html',
   styleUrls: ['./lecture4.component.scss']
 })
-export class Lecture4Component {
+export class Lecture4Component implements OnInit {
 
-  constructor() { }
+  constructor( public authService: AuthService ) { }
+
+  ngOnInit () {
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
 
 }
