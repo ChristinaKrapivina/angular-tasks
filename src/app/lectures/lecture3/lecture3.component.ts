fd99a68a99a160366bc7f8597029acd8adbde823
Inna Lashchenko <Inna_Lashchenko@epam.com>
2020-07-13 22:20:10 +0300

    Lecture 10

--- src/app/lectures/lecture3/lecture3.component.ts
+++ src/app/lectures/lecture3/lecture3.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Output, Input } from '@angular/core';
-import { Observable, Subscription, Observer, Subject } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-lecture3',
@@ -9,15 +9,7 @@ import { Observable, Subscription, Observer, Subject } from 'rxjs';
 export class Lecture3Component implements OnInit {
   // evenNumbers:number[] = [];
   // oddNumbers: number[] = [];
-  parentSubject = new Subject<string>();
-  count$: Observable<number> = new Observable(observer => {
-    let count = 1;
-    setInterval(() => {
-      if (count > 10) observer.complete();
-      observer.next(count);
-      count++;
-    }, 1000)
-  });
+  count$: Observable<number>;
 
   constructor() { }
 
@@ -25,7 +17,16 @@ export class Lecture3Component implements OnInit {
   }
 
   start() {
-    this.parentSubject.next('start');
+    this.count$ = new Observable(observer => {
+      let count = 1;
+      setInterval(() => {
+        if (count > 10) {
+          observer.complete();
+        }
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
     // if(number % 2 === 0) {
     //   this.evenNumbers.push(number);
     // } else {
--- src/app/lectures/lecture3/odd/odd.component.ts
+++ src/app/lectures/lecture3/odd/odd.component.ts
@@ -1,38 +1,27 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
-import { Observable, Observer, Subject, Subscription } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
-import { typeWithParameters } from '@angular/compiler/src/render3/util';
+import {Component, Input, OnDestroy} from '@angular/core';
+import {Observable, Subject, Subscription} from 'rxjs';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-odd',
   templateUrl: './odd.component.html',
   styleUrls: ['./odd.component.scss']
 })
-export class OddComponent implements OnInit {
-  // @Input() inputSubject: Subject<number>;
-  // oddObserver: Observer<number>;
+export class OddComponent implements OnDestroy {
   private subscription: Subscription;
-  @Input() count: Observable<number>;
-  @Input() subject: Subject<string>;
-
   oddNumbers: number[] = [];
   number: number;
 
-  constructor() { }
+  @Input() set count(obs: Observable<number>) {
+    if (!obs) return;
 
-  ngOnInit(): void {
-    this.subscription = this.count
-      .pipe(
-        filter(num => num % 2 !== 0)
-      )
-      .subscribe(num => this.oddNumbers.push(num))
-    // this.subject = this.inputSubject;
-    // this.oddObserver = {
-    //   next: num => console.log(num),
-    //   error: err => console.log(err),
-    //   complete: () => console.log('Observer got a complete no')
-    // }
-    // this.subject.subscribe(this.oddObserver);
+    this.subscription = obs
+      .pipe(filter(num => num % 2 !== 0))
+      .subscribe(num => this.oddNumbers.push(num));
   }
+  @Input() subject: Subject<string>;
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
